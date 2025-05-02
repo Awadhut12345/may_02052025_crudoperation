6df30820bc3c74119d361db6169afe4b8cc4262b
Awadhut <awadhutyenkar@gmail.com>
2025-05-02 11:07:58 +0530

    basic crud operation

---
+++ src/main/java/com/soft/May02052025CrudoperationApplication.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class May02052025CrudoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(May02052025CrudoperationApplication.class, args);
+	}
+
+}
