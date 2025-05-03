bdff90c62e65109474be8a19a0a985c384b1228b
Awadhut <awadhutyenkar@gmail.com>
2025-05-03 16:01:34 +0530

    Exception handle

---
+++ src/main/java/com/soft/exception/GlobleExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.soft.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.soft.DTO.ErrorResponse;
+
+
+@ControllerAdvice
+public class GlobleExceptionHandler {
+	
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+		ErrorResponse error = new ErrorResponse(
+				HttpStatus.NOT_FOUND.value(),
+				ex.getMessage(),
+				LocalDateTime.now()			
+				);
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+		ErrorResponse error = new ErrorResponse(
+				HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				"Intrnal Server Error",
+				LocalDateTime.now()		
+				
+		);		
+		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+
+	
+	
+
+}
