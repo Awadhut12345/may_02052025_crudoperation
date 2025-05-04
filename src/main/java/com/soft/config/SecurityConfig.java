b2b1b1260f9eb9e44a8165fc5a23d2280ab292b8
Awadhut <awadhutyenkar@gmail.com>
2025-05-04 15:09:27 +0530

    Add Spring Security

---
+++ src/main/java/com/soft/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.soft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	
+		http
+			.csrf().disable()
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers("/public/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin()
+			.and()
+			.logout();
+		
+		return http.build();
+	}
+	
+	//User Security
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails user = User.builder()
+				.username("user")
+				.password(encoder.encode("user123"))
+				.roles("USER")
+				.build();
+		
+		
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password(encoder.encode("admin123"))
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	
+	
+	
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+
+}
---
+++ src/main/java/com/soft/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.soft.service;
+
+import com.soft.entity.Admin;
+
+public interface AdminService {
+	
+	public Admin createAdminService(Admin admin);
+
+}
