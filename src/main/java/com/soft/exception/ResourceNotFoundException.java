eb81b49ead36b166e07bd4033e17b8a0b99d1bcc
saurabhaage <aagesaurabh12@gmail.com>
2025-05-06 13:01:27 +0530

    changes

--- src/main/java/com/soft/exception/ResourceNotFoundException.java
+++ src/main/java/com/soft/exception/ResourceNotFoundException.java
@@ -1,10 +1,11 @@
 package com.soft.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
 
-	
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+    
 	
 }
