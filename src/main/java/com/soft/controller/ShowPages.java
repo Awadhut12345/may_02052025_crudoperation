d45929aaab638270b28aae1b06473b1f49a3e772
saurabhaage <aagesaurabh12@gmail.com>
2025-05-07 16:19:42 +0530

    add Employee Registration page

---
+++ src/main/java/com/soft/controller/ShowPages.java
@@ -0,0 +1,13 @@
+package com.soft.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShowPages {
+
+	@GetMapping("/rgistration")
+	public String employeeregistrationpage() {
+		return "employeeregister";
+	}
+}
